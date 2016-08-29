cbea45c8ef32d7b19689865cfedbab01b1ef1207
Kevin Matthews <kmatthews@cyberdata.net>
2016-08-29 14:38:38 -0700

    Cleanup and documentation

--- inc/SocketPrivate.h
+++ inc/SocketPrivate.h
@@ -3,16 +3,42 @@
 
 #include "Socket.h"
 
-// ? Might want to add an options void pointer.
-// Will eventually need to change the file descriptor from an in to a struct, probably.
+/*
+ * All wrappers modules must implement this interface.
+ */
+
+/*
+ * Client and server calls
+ */
+// Return >= 0 file descriptor on success, < 0 on failure.
 typedef int (*SocketOpen)(void);
-typedef void (*SocketClose)(int);
+
+typedef void (*SocketClose)(int file_descriptor);
+
+// Returns the number of bytes sent, or < 0 on failure.
 typedef int (*SocketSend)(int file_descriptor, const char * message, unsigned int message_length);
+
+// Returns the number of bytes received, or < 0 on failure. 
 typedef int (*SocketReceive)(int file_descriptor, char * buffer, unsigned int buffer_length);
+
+/*
+ * Client calls
+ */
+// Return 0 on success, < 0 on failure.
 typedef int (*SocketConnect)(int file_descriptor, const char * ip_address, int port);
+
+/*
+ * Server calls
+ */
+// Return 0 on success, < 0 on failure.
 typedef int (*SocketBind)(int file_descriptor, const char * ip_address, int port);
-typedef int (*SocketAccept)(int file_descriptor);
+
+// Return 0 on success, < 0 on failure.
 typedef int (*SocketListen)(int file_descriptor, int backlog);
+
+// Return 0 on success, < 0 on failure.
+typedef int (*SocketAccept)(int file_descriptor);
+
 typedef struct SocketInterfaceStruct
 {
     SocketOpen open;
