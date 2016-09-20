926dfbafa5fd7b8669041e29ca4142d1e16ff04f
Kevin Matthews <kmatthews@cyberdata.net>
2016-09-19 17:57:45 -0700

    Create basic test list

--- tests/Socket/Test_Socket.cpp
+++ tests/Socket/Test_Socket.cpp
@@ -21,6 +21,63 @@ TEST_GROUP(Socket)
     }
 };
 
+/*
+ * Test List:
+ *  Create:
+ *
+ *  Destroy:
+ *      Null pointers.
+ *      Double destroy.
+ *
+ *  Open:
+ *      Can fail.
+ *      Will not crash with null pointer.
+ *
+ *  Close:
+ *      Can fail.
+ *      Will not crash with null pointer.
+ *
+ *  Connect (client only):
+ *      Can fail.
+ *      Will not crash with null pointer.
+ *
+ *  Bind (server only):
+ *      Can fail.
+ *      Will not crash with null pointer.
+ *
+ *  Listen (server only):
+ *      Can fail.
+ *      Will not crash with null pointer.
+ *
+ *  Accept (server only):
+ *      Can fail.
+ *      Will not crash with null pointer.
+ *
+ *  Send:
+ *      Can fail.
+ *      Will not crash with null pointer.
+ *
+ *  Receive:
+ *      Can fail.
+ *      Will not crash with null pointer.
+ *
+ *  Get handle [file descriptor]:
+ *      Can fail.
+ *      Will not crash with null pointer.
+ *
+ *  Get IP address:
+ *      Can fail.
+ *      Will not crash with null pointer.
+ *
+ *  Get port:
+ *      Can fail.
+ *      Will not crash with null pointer.
+ *
+ *  Set option:
+ *      Can fail.
+ *      Will not crash with null pointer.
+ */
+
 TEST(Socket, it_can_create_and_destroy_a_socket)
 {
 }
